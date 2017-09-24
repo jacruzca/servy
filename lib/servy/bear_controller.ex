3538ba3a70f102bdd56922a5912e4760a705248a
jhon.cruz <jhon@beepquest.com>
2017-09-24 18:27:22 -0500

    Initial commit

---
+++ lib/servy/bear_controller.ex
@@ -0,0 +1,37 @@
+defmodule Servy.BearController do
+
+alias Servy.Conv
+alias Servy.Bear
+alias Servy.Wildthings
+
+@templates_path Path.expand("../../templates", __DIR__)
+
+defp render(conv, template, bindings \\ []) do
+  content =
+    @templates_path
+    |> Path.join(template)
+    |> EEx.eval_file(bindings)
+
+
+    %Conv{conv | status: 200, resp_body: content}
+end
+
+def index(conv) do
+    bears =
+      Wildthings.list_bears()
+      |> Enum.sort(&Bear.order_asc_by_name/2)
+
+      render(conv, "index.eex", bears: bears)
+end
+
+def show(conv, %{"id" => id}) do
+  bear = Wildthings.get_bear(id)
+
+  render(conv, "show.eex", bear: bear)
+end
+
+def create(conv, %{"name" => name, "type"=>type}) do
+  %Conv{conv | status: 201, resp_body: "Created a #{type} named #{name} bear!"}
+end
+
+end
